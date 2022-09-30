2ecab98849f3a232eb7b09df65f1a9762d6f162f
lishabates <112971059+lishabates@users.noreply.github.com>
2022-09-30 11:53:42 -0500

    app_trader_initial_push

---
+++ data/app_tradercodes.sql
@@ -0,0 +1,7 @@
+a. Develop some general recommendations as to the price range, genre, content rating, or anything else for apps that the company should target.
+select * from play_store_apps;
+
+select * 
+from app_store_apps as a 
+INNER JOIN play_store_apps as p 
+where 
