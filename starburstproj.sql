592bf9156ac32a1d070afe5bcf49f9a617a5bfba
scost46 <scost46@gmail.com>
2022-10-03 10:26:58 -0500

    final starburt push

--- starburstproj.sql
+++ starburstproj.sql
@@ -1,11 +1,13 @@
-SELECT name,
+SELECT a.name,
  ROUND((a.rating+p.rating)/2,2) AS avg_rating,
 (a.price::money + p.price::money)/2 AS avg_price,
-(a.review_count::integer+p.review_count::integer)/2 AS avg_review_count,genres
+((a.price::money + p.price::money)/2 * 10000) AS purchase_price,
+(a.review_count::integer+p.review_count::integer)/2 AS avg_review_count,
+genres
 FROM app_store_apps AS a
 INNER JOIN play_store_apps AS p
 USING(name)
 WHERE type = 'Paid'
-GROUP BY name,avg_rating,avg_price,avg_review_count, genres
+GROUP BY a.name,avg_rating,avg_price,avg_review_count,genres
 HAVING ROUND((a.rating+p.rating)/2,2) >= 4
 ORDER BY avg_review_count DESC;
